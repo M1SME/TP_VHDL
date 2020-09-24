ee881343b86943fdec403d415d0ab0f31424388d
Frederic JA <frederic.jenn.alet@gmail.com>
2020-09-24 11:19:56 +0200

    test

---
+++ CPU_test/mon_SOPC_inst.vhd
@@ -0,0 +1,10 @@
+  --Example instantiation for system 'mon_SOPC'
+  mon_SOPC_inst : mon_SOPC
+    port map(
+      out_port_from_the_OutputPIO => out_port_from_the_OutputPIO,
+      clk_0 => clk_0,
+      in_port_to_the_InputPIO => in_port_to_the_InputPIO,
+      reset_n => reset_n
+    );
+
+
