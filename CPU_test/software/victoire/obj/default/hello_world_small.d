7ee46435726140fb3b4e651cc9b2d988ae2e8a78
Cyprien <cyprien.quivet@gmail.com>
2020-09-27 17:11:49 +0200

    MAJ Reparation GIT

--- CPU_test/software/victoire/obj/default/hello_world_small.d
+++
@@ -1,4 +0,0 @@
-obj/default/hello_world_small.o: hello_world_small.c \
-  ../victoire_bsp//HAL/inc/sys/alt_stdio.h
-
-../victoire_bsp//HAL/inc/sys/alt_stdio.h:
---
+++ NIOS/CPU_test/software/Test/obj/default/hello_world_small.d
@@ -0,0 +1,4 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+  ../Test_bsp//HAL/inc/sys/alt_stdio.h
+
+../Test_bsp//HAL/inc/sys/alt_stdio.h:
