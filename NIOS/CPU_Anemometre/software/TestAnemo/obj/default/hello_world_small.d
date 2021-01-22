656579fbbfef89ff9372b230cb4ee220a56f18ed
Cyprien <cyprien.quivet@gmail.com>
2021-01-22 10:49:39 +0100

    MaJ changement PC

---
+++ NIOS/CPU_Anemometre/software/TestAnemo/obj/default/hello_world_small.d
@@ -0,0 +1,9 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+  ../TestAnemo_bsp/system.h ../TestAnemo_bsp/linker.h \
+  ../TestAnemo_bsp//HAL/inc/sys/alt_stdio.h
+
+../TestAnemo_bsp/system.h:
+
+../TestAnemo_bsp/linker.h:
+
+../TestAnemo_bsp//HAL/inc/sys/alt_stdio.h:
---
+++ NIOS/CPU_Boussole/software/TestBoussole/obj/default/hello_world_small.d
@@ -0,0 +1,9 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+  ../TestBoussole_bsp/system.h ../TestBoussole_bsp/linker.h \
+  ../TestBoussole_bsp//HAL/inc/sys/alt_stdio.h
+
+../TestBoussole_bsp/system.h:
+
+../TestBoussole_bsp/linker.h:
+
+../TestBoussole_bsp//HAL/inc/sys/alt_stdio.h:
